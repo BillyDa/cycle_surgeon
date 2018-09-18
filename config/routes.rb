fa983b23031de857c92b07df9a500b345f059d9a
“Tyler <tylerpalef@gmail.com>
2018-09-18 11:34:34 -0400

    made a few changes to our gem file. had to un-comment turbolinks, as it was cuasing a conflict in our application_controller (controller). created WIP form for the new user, as well as the ticket. also updated routes to include users, as that was required to create a user

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,10 @@ get 'login' => 'user_sessions#new', :as => :login
 delete 'logout' => 'user_sessions#destroy', :as => :logout
 
 resources :user, only: [:new, :create, :show]
-# Cyclist.. if logged in
-# Go to tickets#new
+
+resources :users
 
 resources :tickets, only: [:new, :create, :show, :delete]
-# Surgeon... if logged in
-# go to tickets#show
+
 
 end
