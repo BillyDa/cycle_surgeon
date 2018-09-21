f8a141e0c09a6ce86ea9cd690cf599733c4f9400
“Tyler <tylerpalef@gmail.com>
2018-09-21 17:50:18 -0400

    working on the geo location. going to try and stored the data. need to rename the model

--- app/assets/javascripts/alert.js
+++ app/assets/javascripts/alert.js
@@ -1,5 +1,55 @@
-// navigator.geolocation.getCurrentPosition(function(position){
-//   do_something(position.coords.latitude, position.coords.longitude);
+
+//
+// var x = navigator.geolocation.getCurrentPosition(function(position) {
+//    position.coords.latitude;
 // });
 //
-// something = console.log(doSomething);
+// var y = navigator.geolocation.getCurrentPosition(function(position) {
+//    position.coords.longitude;
+// });
+
+navigator.geolocation.getCurrentPosition(function(positionSuccess){
+  var lon = positionSuccess.coords.longitude;
+  var lat = positionSuccess.coords.latitude;
+  console.log(lon)
+  console.log(lat)
+  $.ajax({
+    type: "POST",
+    url: "/controller/tickets"
+    data: { user_latitude: lat, user_longitude: lon}
+  })
+  }
+  ,function (error) {
+  console.log(error)
+})
+
+//
+//
+// function geoFindMe() {
+//   var output = document.getElementById("out");
+//
+//   if (!navigator.geolocation){
+//     output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
+//     return;
+//   }
+//
+//   function success(position) {
+//     var latitude  = position.coords.latitude;
+//     var longitude = position.coords.longitude;
+//
+//     output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
+//
+//     var img = new Image();
+//     img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
+//
+//     output.appendChild(img);
+//   }
+//
+//   function error() {
+//     output.innerHTML = "Unable to retrieve your location";
+//   }
+//
+//   output.innerHTML = "<p>Locating…</p>";
+//
+//   navigator.geolocation.getCurrentPosition(success, error);
+// }
