0fc9db6d85ec1f3966983c4357484f98224842aa
“Tyler <tylerpalef@gmail.com>
2018-09-24 11:22:22 -0400

    Updates to new ticket form for repair types

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -6,6 +6,42 @@ validates :repair, presence: true
 
 has_many :users
 
+def self.repair_types
+    return {
+      0 => "Hydraulic Disc",
+      1 => "Handle Bar",
+      2 => "Brake",
+      3 => "Saddle",
+      4 => "Lever",
+      5 => "V Brake",
+      6 => "Flat Tire",
+      7 => "Tune Up",
+      8 => "Other"
+    }
+end
+
+def repair_string
+  case repair
+  when 0
+    return "Hydraulic Disc"
+  when 1
+    return "Handle Bar"
+  when 2
+    return "Brake"
+  when 3
+    return "Saddle"
+  when 4
+    return "Lever"
+  when 5
+    return "V Brake"
+  when 6
+    return "Flat Tire"
+  when 7
+    return "Tune Up"
+  when 8
+    return "Other"
+  end
+end
 # Tickets belong to a user
 end
 #
