6f41eea70100cb977e7c676f1cf97552314f7d30
Billy Davidson <billy.davids52@gmail.com>
2018-09-24 19:58:04 -0400

    fixed show page weirdness, ignore lines 13-15

--- app/controllers/accepted_controller.rb
+++ app/controllers/accepted_controller.rb
@@ -5,6 +5,7 @@ class AcceptedController < ApplicationController
     @user = current_user
     # @ticket.ticket.repair_types = Ticket.find(params[:id])
     @ticket_repair_string = Ticket.repair_types[@ticket.id]
+    @user_surgeon_tick = Ticket.all
 
   end
 end
