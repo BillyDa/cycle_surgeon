e1ebd1041e45920b436d8eaed6ef0d6eb7381956
schwarro <rob.ap.schwartz@gmail.com>
2018-09-27 12:33:04 -0400

    added styling to images when selecting role

--- app/assets/javascripts/sign_up.js
+++ app/assets/javascripts/sign_up.js
@@ -3,11 +3,30 @@ document.addEventListener("DOMContentLoaded", function(event) {
   {
     var checkbox = document.getElementById("user_surgeon");
     checkbox.checked = true;
+    this.style.border = "3px solid grey";
+    var cycle = document.getElementById("cycle_image")
+    cycle.style.border = "";
   }
 
   document.getElementById("cycle_image").onclick = function()
   {
     var checkbox = document.getElementById("user_surgeon");
     checkbox.checked = false;
+    this.style.border = "3px solid grey";
+    var surgeon = document.getElementById("surgeon_image")
+    surgeon.style.border = "";
   }
 });
+
+// document.addEventListener("DOMContentLoaded", function(event) {
+//   console.log("Hello")
+//   var images = document.getElementById("surgeon_image").onclick = function()
+//   for (var i = 0; i < images.length; i++) {
+//     images[i].addEventListener('click',function(e){
+//       for (var i = 0; i < images.length; i++) {
+//         images[i].children[0].lastElementChild.style.border = "";
+//       }
+//       e.target.style.border = "5px solid black";
+//     })
+//   }
+//  });
